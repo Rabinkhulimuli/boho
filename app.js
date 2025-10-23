76587d96af304f654d771ab5f4da035cd8274511
Rabin Khulimuli <131673248+Rabinkhulimuli@users.noreply.github.com>
2025-10-23 10:58:36 +0545

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const flash = require("connect-flash");
 
 require("dotenv").config();
 
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
 const ownersRouter = require("./routes/ownersRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
