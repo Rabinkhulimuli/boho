3849b4a36cdaaf8c73d69fa9f2f4f05b7eb91c87
Rabin Khulimuli <131673248+Rabinkhulimuli@users.noreply.github.com>
2025-10-23 10:41:28 +0545

    Refactor mongoose connection to use MONGO_URL
    Updated MongoDB connection to use environment variable for URI.

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
-const config = require("config");
-
+const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 
+// Get MongoDB URI from environment variable
+const mongoUrl = process.env.MONGO_URL;
+
+if (!mongoUrl) {
+  console.error(" MONGO_URL is not defined. Please set it in Vercel Environment Variables.");
+  process.exit(1);
+}
+
 mongoose
-.connect(`${config.get("MONGO_URL")}/premiumbagshop`)   
-.then(function(){
-    dbgr("connected");   
-})
-.catch(function(err){
-    dbgr(err); 
-});
+  .connect(`${mongoUrl}/premiumbagshop`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => dbgr(" Connected to MongoDB"))
+  .catch((err) => dbgr(" MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
